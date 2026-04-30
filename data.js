feca59d8a080db22deaa594d4264ba6e927929fe
moonbrooke <smoeland@gmail.com>
2026-04-30 17:47:18 +0700

    dataaaa

---
+++ data.js
@@ -0,0 +1,17 @@
+const projectsData = {
+    "1": {
+        "title": "FIFA Points Calculator",
+        "url": "/fifa-points-calc",
+        "description": "How many points teams earned/lost after a match"
+    },
+    "2": {
+        "title": "Link 2",
+        "url": "/link2",
+        "description": "this is description 2"
+    },
+    "3": {
+        "title": "Link 3",
+        "url": "/link3",
+        "description": "this is description 3"
+    }
+};
