d914678e9ca29fecd1f40445ead2b433edbde4b3
moonbrooke <smoeland@gmail.com>
2026-05-24 20:01:18 +0700

    add icons

--- data.js
+++ data.js
@@ -1,16 +1,19 @@
 const projectsData = {
     "1": {
         "title": "FIFA Points Calculator",
+        "icon": "⚽",
         "url": "https://moonbrooke.github.io/fifa-points-calc",
         "description": "How many points teams earned/lost after a match"
     },
     "2": {
         "title": "Hunker Down Explorer",
+        "icon": "🏕️",
         "url": "https://moonbrooke.github.io/hunker-down-explorer",
         "description": "Explore Steve Wallis's hunker down spots"
     },
     "3": {
         "title": "Minecraft - Fast Smelt Data Pack",
+        "icon": "⛏️",
         "url": "https://github.com/moonbrooke/fast-smelt-datapack",
         "description": "Smelt items on Furnace, Blast Furnace, Smoker, and Campfire FAST!"
     }
