5990c40d4cb94cbe8a89a1d20d0e07bd18d26250
moonbrooke <smoeland@gmail.com>
2026-05-24 14:44:49 +0700

    data: add fast-smelt-datapack

--- data.js
+++ data.js
@@ -1,17 +1,17 @@
 const projectsData = {
     "1": {
         "title": "FIFA Points Calculator",
-        "url": "/fifa-points-calc",
+        "url": "https://moonbrooke.github.io/fifa-points-calc",
         "description": "How many points teams earned/lost after a match"
     },
     "2": {
         "title": "Hunker Down Explorer",
-        "url": "/hunker-down-explorer",
+        "url": "https://moonbrooke.github.io/hunker-down-explorer",
         "description": "Explore Steve Wallis's hunker down spots"
     },
     "3": {
-        "title": "Link 3",
-        "url": "/link3",
-        "description": "this is description 3"
+        "title": "Minecraft - Fast Smelt Data Pack",
+        "url": "https://github.com/moonbrooke/fast-smelt-datapack",
+        "description": "Smelt items on Furnace, Blast Furnace, Smoker, and Campfire FAST!"
     }
 };
