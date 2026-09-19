ef44d47ffd61291debb191f834f72b7a75a73272
moonbrooke <smoeland@gmail.com>
2026-09-19 17:37:15 +0700

    typo

--- data.js
+++ data.js
@@ -36,7 +36,7 @@ const projectsData = {
         "description": "Extract thumbnails from YouTube videos"
     },
     "7": {
-        "title": "Moiz",
+        "title": "Noiz",
         "icon": "📻",
         "url": "https://moonbrooke.github.io/noiz",
         "description": " A very simple web based (local) music player"
